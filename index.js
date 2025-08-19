55ae5ce5d3ff78ad15574ed48e903cb7cd339d08
Chathuranga <cnadeeshan90@gmail.com>
2025-08-19 19:49:24 +0530

    day 13

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import studentRouter from "./routers/studentsRouter.js";
 import userRouter from "./routers/userRouter.js";
 import jwt from "jsonwebtoken";
 import productRouter from "./routers/productRouter.js";
+import cors from "cors";
+import dotenv, { configDotenv } from "dotenv";
+
+dotenv.config();
+
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 app.use(
